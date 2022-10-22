58fe9f11f217619dfe5ffd5b6019b10304d5d5c1
Roger David Alba Ortega <99060136+Davidalbort@users.noreply.github.com>
2022-10-22 17:19:30 -0500

    Change architectura of folder and add config of eslint and tailwind css

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'overrides': [
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'max-len': ['error', { 'code': 80 }],
+		'react/react-in-jsx-scope': 'off',
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
